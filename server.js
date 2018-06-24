95fddae047b81e49bea4861634ce3da5cffe966d
Ahmed Alaa El-Din <ahmedandaloesg@gmail.com>
2018-06-24 03:14:13 +0200

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 
@@ -62,6 +63,6 @@ app.get('/bad', (req,res) => {
 });
 
 
-app.listen(3000, () => { 
+app.listen(port, () => { 
     console.log('server is up at port 3000 succes');
 });
